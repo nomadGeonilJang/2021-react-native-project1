e8c951a01bfb2813ab3ac5836f1601b5e328c665
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-01-06 23:16:57 +0900

    Add Location State

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import React, { useState,useEffect } from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import * as Location from "expo-location";
+
+
 import Loading from "./Loading"
 
 export default function App() {
-  return (
-    <Loading/>
-  );
+
+  const [state, setState] = useState({
+    isLoading:true,
+  })
+
+  const getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync()
+      const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync()
+      setState(prev=>({...prev, isLoading:false}))
+      console.log(latitude, longitude)
+    } catch (error) {
+      Alert.alert("Cant't find you")
+    }
+  }
+
+  useEffect(() => {
+    getLocation()
+  }, [])
+
+
+
+  return (state.isLoading ? <Loading/> : null)
 }
 
 const styles = StyleSheet.create({
